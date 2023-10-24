a9e240134d7bd00fd6ed9a0ac7fc6d5e5323e4cf
rebecca-and <115492818+rebecca-and@users.noreply.github.com>
2023-10-24 10:50:19 +0100

    Update basic_multinom.R

--- R/basic_multinom.R
+++ R/basic_multinom.R
@@ -2,88 +2,88 @@
 library(nnet)
 library(tidyverse)
 
-nga <- readRDS("C:/Users/rla121/Imperial College London/HIV Inference Group - WP - Documents/Data/KP/Individual level data/NGA/NGA2020BBS_FSW/NGA2020BBS_FSW.rds")
-nga <- nga %>% 
-  mutate(age2 = age,
-  age = as.factor(age))
-
-model <- multinom(age_group ~ 1 + year * iso , data = new_agedata)
-
-plot(model$fitted.values[1,])
-
-age_values <- as.factor(seq(from = min(nga$age2), to = max(nga$age2), by = 1))
-predicted_probs <- predict(model, newdata = data.frame(age = age_values), type = "probs")
-total_counts <- 4974 
-expected_counts <- data.frame(predicted_probs * total_counts)
-
-expected_counts <- expected_counts[1,] %>% 
-  pivot_longer(cols = everything(), names_to = "age", values_to = "count") %>% 
-  mutate(id.age = row_number())
-
-
-nga2 <- nga %>% 
-  group_by(age2) %>% 
-  mutate(count = n()) %>% 
-  ungroup() %>% 
-  select(age2, count) %>% 
-  distinct() %>% 
-  arrange(age2) %>% 
-  mutate(age.id = row_number()) %>% 
-  select(age.id, count)
-
-ggplot(expected_counts, aes(x = id.age, y = count)) +
-  geom_line() +
-  labs(x = "Age", y = "Number of People", color = "Age Group") +
-  scale_color_discrete(name = "Age Group") +
-  theme_minimal() + 
-  geom_line(aes(x = nga2$age.id, y = nga2$count, color = "orange"))
-
-
-
-# mnl_pred_ova(model = model,
-#              data = ages_to_predict,
-#              x = "age",
-#              by = 1,
-#              seed = "random", # default
-#              nsim = 100, # faster
-#              probs = c(0.025, 0.975))
-# # Print the results
-# print(results)
-
-
-model <-VGAM::vglm(age ~ 1, family = multinomial, data = nga)
-plot(model@fitted.values[1,])
-
-
-# predict(model, newdata = data.frame(age = age_values), type = "probs")
-# VGAM::predict.vglm(model, newdata = data.frame(age = agevalues), type = "response", se.fit = T)
-
-prediction <- predictvglm(object = model, newdata = data.frame(age = age_values), 
-             type =  "link", se.fit = T)
-
-plot(prediction[1,])
-
-full_dat
-a <-  data.frame(prediction[["fitted.values"]][1,],  prediction[["se.fit"]][1,]) %>% 
-  mutate(age.id = row_number()) %>% 
-  rename(logestimate = prediction...fitted.values....1..., se = prediction...se.fit....1...) %>% 
-  mutate(loglower = logestimate - 1.96*se,
-         logupper = logestimate + 1.96*se, 
-         lower = exp(loglower),
-         upper = exp(logupper),
-         estimate = exp(logestimate)) 
-
-nga3 <- nga2 %>% 
-  filter(!age.id == 48)
-
-full_data %>% 
-  ggplot() + 
-  geom_line(aes(x = age.id, y = estimate)) +
-  geom_ribbon(aes(x = age.id, ymin = lower, ymax = upper), alpha = 0.2) + 
-  moz.utils::standard_theme() +
-  geom_point(aes(x = nga3$age.id, y = nga3$count)) + 
-  coord_cartesian(ylim = c(0, 1000))
-
+# nga <- readRDS("C:/Users/rla121/Imperial College London/HIV Inference Group - WP - Documents/Data/KP/Individual level data/NGA/NGA2020BBS_FSW/NGA2020BBS_FSW.rds")
+# nga <- nga %>% 
+#   mutate(age2 = age,
+#   age = as.factor(age))
+# 
+# model <- multinom(age_group ~ 1 + year * iso , data = new_agedata)
+# 
+# plot(model$fitted.values[1,])
+# 
+# age_values <- as.factor(seq(from = min(nga$age2), to = max(nga$age2), by = 1))
+# predicted_probs <- predict(model, newdata = data.frame(age = age_values), type = "probs")
+# total_counts <- 4974 
+# expected_counts <- data.frame(predicted_probs * total_counts)
+# 
+# expected_counts <- expected_counts[1,] %>% 
+#   pivot_longer(cols = everything(), names_to = "age", values_to = "count") %>% 
+#   mutate(id.age = row_number())
+# 
+# 
+# nga2 <- nga %>% 
+#   group_by(age2) %>% 
+#   mutate(count = n()) %>% 
+#   ungroup() %>% 
+#   select(age2, count) %>% 
+#   distinct() %>% 
+#   arrange(age2) %>% 
+#   mutate(age.id = row_number()) %>% 
+#   select(age.id, count)
+# 
+# ggplot(expected_counts, aes(x = id.age, y = count)) +
+#   geom_line() +
+#   labs(x = "Age", y = "Number of People", color = "Age Group") +
+#   scale_color_discrete(name = "Age Group") +
+#   theme_minimal() + 
+#   geom_line(aes(x = nga2$age.id, y = nga2$count, color = "orange"))
+# 
+# 
+# 
+# # mnl_pred_ova(model = model,
+# #              data = ages_to_predict,
+# #              x = "age",
+# #              by = 1,
+# #              seed = "random", # default
+# #              nsim = 100, # faster
+# #              probs = c(0.025, 0.975))
+# # # Print the results
+# # print(results)
+# 
+# 
+# model <-VGAM::vglm(age ~ 1, family = multinomial, data = nga)
+# plot(model@fitted.values[1,])
+# 
+# 
+# # predict(model, newdata = data.frame(age = age_values), type = "probs")
+# # VGAM::predict.vglm(model, newdata = data.frame(age = agevalues), type = "response", se.fit = T)
+# 
+# prediction <- predictvglm(object = model, newdata = data.frame(age = age_values), 
+#              type =  "link", se.fit = T)
+# 
+# plot(prediction[1,])
+# 
+# full_dat
+# a <-  data.frame(prediction[["fitted.values"]][1,],  prediction[["se.fit"]][1,]) %>% 
+#   mutate(age.id = row_number()) %>% 
+#   rename(logestimate = prediction...fitted.values....1..., se = prediction...se.fit....1...) %>% 
+#   mutate(loglower = logestimate - 1.96*se,
+#          logupper = logestimate + 1.96*se, 
+#          lower = exp(loglower),
+#          upper = exp(logupper),
+#          estimate = exp(logestimate)) 
+# 
+# nga3 <- nga2 %>% 
+#   filter(!age.id == 48)
+# 
+# full_data %>% 
+#   ggplot() + 
+#   geom_line(aes(x = age.id, y = estimate)) +
+#   geom_ribbon(aes(x = age.id, ymin = lower, ymax = upper), alpha = 0.2) + 
+#   moz.utils::standard_theme() +
+#   geom_point(aes(x = nga3$age.id, y = nga3$count)) + 
+#   coord_cartesian(ylim = c(0, 1000))
+# 
 
 
 basic_age <- readxl::read_xlsx("data (public)/basic_age_mod.xlsx") %>% 
@@ -194,7 +194,7 @@ prediction_data4 <- basic_age %>%
                                                               age_group == "X4" ~ 4,
                                                               age_group == "X5" ~ 5)))
 
-iso_fe <- prediction_data4 %>% 
+prediction_data4 %>% 
   ggplot(aes(x = age_group)) + 
   geom_line(aes(y = prob)) + 
   geom_point(aes(y = prop)) + 
@@ -217,7 +217,7 @@ prediction_data5 <- basic_age %>%
                                                               age_group == "X4" ~ 4,
                                                               age_group == "X5" ~ 5)))
 
-year_iso_inter <- prediction_data5 %>% 
+prediction_data5 %>% 
   ggplot(aes(x = age_group)) + 
   geom_line(aes(y = prob)) + 
   geom_point(aes(y = prop)) + 
@@ -337,17 +337,17 @@ prediction_data6 %>%
 library(Matrix)
 library(TMB)
 
-mf_model <- crossing(
-  iso = c(1,2,3),
-  age_group = c(1,2,3,4,5)
-,
-year = c(2006, 2009, 2011, 2014, 2017)) %>% 
-  mutate(iso = factor(iso),
-         age_group = factor(age_group),
-         idx = factor(row_number()))
-
-new_agedata <- new_agedata %>% 
-  left_join(mf_model)
+# mf_model <- crossing(
+#   iso = c(1,2,3),
+#   age_group = c(1,2,3,4,5)
+# ,
+# year = c(2006, 2009, 2011, 2014, 2017)) %>% 
+#   mutate(iso = factor(iso),
+#          age_group = factor(age_group),
+#          idx = factor(row_number()))
+# 
+# new_agedata <- new_agedata %>% 
+#   left_join(mf_model)
 
 basic_age <- basic_age %>% 
   mutate(idx = factor(row_number()),
