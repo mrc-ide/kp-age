0f89399128f3f7a5ea6b185fb30152e7f7478c7e
rebecca-and <115492818+rebecca-and@users.noreply.github.com>
2025-05-13 09:45:36 +0100

    latest vls wrangling

--- R/vls_by_age_2904.R
+++ R/vls_by_age_2904.R
@@ -37,33 +37,27 @@ spectrum_vls_dat <- readRDS("~/Downloads/2024_spectrum_data.rds") %>%
   filter(year %in% c(1990:2024)) %>% filter(sex == "female")
 
 
-sup_survs <- dat %>% filter(!is.na(vl_result_suppressed)) %>% distinct(survey_id) %>% pull(survey_id)
+# sup_survs <- dat %>% filter(!is.na(vl_result_suppressed)) %>% distinct(survey_id) %>% pull(survey_id)
 
-vls_dat <- fsw_dat2 %>% 
-  mutate(vl = ifelse(vl %in% c("<20", "INDETECTABLE"), 0, vl)) %>% 
-  type.convert(as.is = T)
+fsw_vls_dat <- fsw_dat2 %>% 
+  mutate(vl_detect = ifelse(vl %in% c("<20", "INDETECTABLE"), 1, 0),
+    vl = extract_numeric(vl),
+    vl = ifelse(is.na(vl) & vl_detect == 1, 0, vl),
+    vl_result_suppressed2 = case_when(vl<1000 ~ 1,
+                                          vl >= 1000 & vl <100000000000 ~ 0,
+                                          is.na(vl) ~ NA_integer_),
+    vl_result_suppressed2 = ifelse(is.na(vl_result_suppressed2), vl_result_suppressed, vl_result_suppressed2),
+    vl_10 = 10^vl_result_log10,
+    vl_result_suppressed2 = case_when(is.na(vl_result_suppressed2) & vl_10 < 1000 ~ 1,
+                                      is.na(vl_result_suppressed2) & vl_10 >= 1000 ~ 0,
+                                      TRUE ~ vl_result_suppressed2),
+    vl_result_suppressed2 = ifelse(is.na(vl_result_suppressed2) & vl_result_detectable == "undetectable", 1, vl_result_suppressed2 )) %>% 
+  select(survey_id, year, kp, hiv, vl_result_suppressed = vl_result_suppressed2,  age, duration_estimate) %>% 
+  filter(!is.na(vl_result_suppressed))  
 
 
-
-  select(survey_id, year, age, duration_estimate, vl) %>% 
-  mutate(vl_result_suppressed = ifelse(vl < 1000, 1, 0)) %>% 
-  bind_rows(
-    dat %>% 
-      filter(!is.na(vl_result_suppressed), kp == "FSW") %>%
-      mutate(duration_calc = age - age_fs_paid,
-             duration_calc = ifelse(duration_calc < 0 , NA_integer_, duration_calc),
-             duration_calc2 = age - age_fs_gift,
-             duration_calc2 = ifelse(duration_calc2 < 0 , NA_integer_, duration_calc2),
-             duration_calc3 = age - age_fs_paidorgift,
-             duration_calc3 = ifelse(duration_calc3 < 0 , NA_integer_, duration_calc3),
-             duration_calc4 = age - age_startsw,
-             duration_calc4 = ifelse(duration_calc4 < 0 , NA_integer_, duration_calc4),
-             duration_yr = ifelse(duration_yr > age, NA_integer_, duration_yr),
-             duration_estimate = ifelse(is.na(duration_yr), duration_calc, duration_yr),
-             duration_estimate = ifelse(is.na(duration_estimate), duration_calc4, duration_estimate),
-             duration_estimate = ifelse(is.na(duration_estimate), duration_calc2, duration_estimate),
-             duration_estimate = ifelse(is.na(duration_estimate), duration_calc3, duration_estimate))
-  ) %>% 
+fsw_vls_age_dat <- fsw_vls_dat %>% 
+  filter(!is.na(age)) %>% 
   group_by(survey_id, year, age, vl_result_suppressed) %>% 
   summarise(n = n()) %>% 
   ungroup() %>% 
@@ -75,17 +69,25 @@ vls_dat <- fsw_dat2 %>%
   left_join(spectrum_vls_dat)
 
 # For no year
-inla_vlsdat <- data.frame(age = 15:60,
+inla_fsw_vlsdat_age <- data.frame(age = 15:60,
                           denom = 1) %>% 
-  bind_rows(vls_dat)
+  bind_rows(fsw_vls_age_dat)
+
+# For no year, certain genpop level
+inla_fsw_vlsdat_age <- crossing(age = 15:60,
+                                tot_treat = c(0.3, 0.75, 0.95),
+                                  denom = 1) %>% 
+  bind_rows(fsw_vls_age_dat)
 
 # With year
-inla_vlsdat <- crossing(age = 15:60, year = 2000:2023) %>% 
+inla_fsw_vlsdat_age <- crossing(age = 15:60, year = 2000:2023) %>% 
   mutate(denom = 1) %>% 
-  bind_rows(vls_dat) %>% 
+  bind_rows(fsw_vls_age_dat) %>% 
   mutate(age_centre = age - 14,
          year_centre = year - 1999) %>% 
   filter(!is.na(age))
+
+
 
 formula = n ~ 1 + age + f(survey_id, model = "iid") 
 
@@ -100,13 +102,13 @@ fsw_vls_mod <- INLA::inla(formula,
                            Ntrials = denom,
                            # E = tot_prev,
                            offset = qlogis(tot_treat),
-                           data = inla_vlsdat,
+                           data = inla_fsw_vlsdat_age,
                            control.compute=list(config = TRUE))
 summary(fsw_vls_mod)
 
-fsw_vlsamples <- moz.utils::sample_model(fsw_vls_mod, inla_vlsdat, "n")
+fsw_vlsamples <- moz.utils::sample_model(fsw_vls_mod, inla_fsw_vlsdat_age, "n")
 
-# without year
+# without year to plot OR
 fsw_vlsamples %>% 
   mutate(plogis_lower = plogis(lower),
          plogis_mean = plogis(mean),
@@ -117,13 +119,22 @@ fsw_vlsamples %>%
   ggplot() +
   geom_line(aes(x = age, y = exp_mean)) + 
   geom_ribbon(aes(x = age, ymin = exp_lower, ymax = exp_upper), alpha = 0.5) +
-  geom_point(data = inla_vlsdat, aes(x = age, y = ((n/denom)/(1-(n/denom)))/((tot_treat)/(1-tot_treat)), color = survey_id), show.legend = F) + 
+  geom_point(data = fsw_vls_age_dat, aes(x = age, y = ((n/denom)/(1-(n/denom)))/((tot_treat)/(1-tot_treat)), color = survey_id), show.legend = F) + 
   moz.utils::standard_theme() + 
   labs(x = "Age", y = "OR for VLS in KP relative to genpop") + 
   scale_y_continuous(trans = "log", labels = scales::number_format(accuracy = 0.1)) + 
   theme(aspect.ratio = 1) + 
-  guides(color = FALSE)
+  guides(color = FALSE) +
+  lims(x = c(15,50)) +
+  geom_hline(yintercept = 1, color = "darkred", linetype = 2)
 
+# without year to plot at certain levels of genpop covg.
+fsw_vlsamples %>% 
+  mutate(plogis_lower = plogis(lower),
+         plogis_mean = plogis(mean),
+         plogis_upper = plogis(upper)
+         
+         
 # with year
 fsw_vlsamples %>% 
   mutate(plogis_lower = plogis(lower),
