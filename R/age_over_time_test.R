63d6cd0b23a819b484a5050c29ee698e56e9d14f
rebecca-and <115492818+rebecca-and@users.noreply.github.com>
2025-09-09 22:20:10 +0100

    Update age_over_time_test.R

--- R/age_over_time_test.R
+++ R/age_over_time_test.R
@@ -39,13 +39,16 @@ inla_fsw_age <- inla_fsw_age %>%
   mutate(year_centre = year-2000) %>% 
   mutate(year_min = year - min(year),
          iso3_id = iso3,
-         mean_year = year - 2017)
+         mean_year = (year - 2017)/5)
 
-fsw_age_formula1 <- age ~ 1 + mean_year + f(iso3, model = "iid") + f(iso3_id, mean_year, model = "iid") + offset(log(genpop_median))
+inla_fsw_age2 <- inla_fsw_age %>% 
+  filter(!year < 2005)
+
+fsw_age_formula1 <- age ~ 1 + mean_year + f(iso3, model = "iid") + f(iso3_id, mean_year, model = "iid") + offset(log(genpop_mean))
 
 fsw_agemod_gamma_offset <- INLA::inla(formula = fsw_age_formula1,
                                       family = "gamma",
-                                      control.compute=list(config = TRUE),
+                                      control.compute=list(config = TRUE, waic = T),
                                       data = inla_fsw_age)
 
 summary(fsw_agemod_gamma_offset)
@@ -58,9 +61,9 @@ fsw_agemod_gamma_offset_samples <- moz.utils::sample_model(fsw_agemod_gamma_offs
            exp_lower = exp(lower),
            exp_upper = exp(upper)) %>% 
     ggplot() + 
-    geom_line(aes(x = year , y = exp(mean)-genpop_median)) +
-    geom_ribbon(aes(x = year, ymin = exp(lower)- genpop_median, ymax = exp(upper)-genpop_median), alpha = 0.3) +
-    geom_point(data = fsw_age_dat , aes(x = year, y = mean_kp_age-genpop_median, size = denom, color = iso3), show.legend = F) + 
+    geom_line(aes(x = year , y = (exp(mean))-genpop_mean)) +
+    geom_ribbon(aes(x = year, ymin = (exp(lower))- genpop_mean, ymax = (exp(upper))-genpop_mean), alpha = 0.3) +
+    geom_point(data = fsw_age_dat , aes(x = year, y = mean_kp_age-genpop_mean, size = denom, color = iso3), show.legend = F) + 
     geom_hline(yintercept = 0 , color = "darkred", linetype = "dotted", linewidth = 0.7) +
     moz.utils::standard_theme() + 
     labs(x = "Year", y = "Number of years older \nthan the total population") +
@@ -181,103 +184,252 @@ msm_agemod_gamma_offset_samples <- moz.utils::sample_model(msm_agemod_gamma_offs
 #     ggtitle("SSA"))
 
 
+# 
+# ## Generate a means 
+# 
+# test <- inla_fsw_age %>% filter(!is.na(survey_id)) %>% 
+#   filter(!is.na(age)) %>% 
+#   group_by(survey_id) %>% 
+#   mutate(mean_kp_age = mean(age),
+#          sd_kp_age = sd(age),
+#          denom = n()) %>% 
+#   ungroup() %>% 
+#   select(survey_id, year, median_kp_age, mean_kp_age, sd_kp_age, denom, genpop_mean) %>% 
+#   distinct() %>% 
+#   # group_by(survey_id, age) %>% 
+#   # mutate(age_denom = n()) %>% 
+#   # ungroup() %>% 
+#   mutate(se = sd_kp_age/sqrt(denom),
+#          variance = se^2,
+#          weights = 1/variance,
+#          diff = mean_kp_age - genpop_mean,
+#          mean_year = year - 2017)  
+# 
+# 
+# weighted_model <- lm(diff ~ mean_year, 
+#                      data = test, 
+#                      weights = weights)
+# 
+# summary(weighted_model)
+# 
+# pred_data <- data.frame(year = 1993:2023) %>% 
+#   mutate(mean_year = year - 2017)
+# predictions <- predict(weighted_model, newdata = pred_data, 
+#                        interval = "confidence", level = 0.95)
+# pred_df <- data.frame(
+#   year = 1993:2023,
+#   estimate = predictions[,"fit"],
+#   lower = predictions[,"lwr"],
+#   upper = predictions[,"upr"]
+# )
+# 
+# test %>% 
+# ggplot() +
+#   geom_point(aes(x = year, y = diff, size = weights), alpha = 0.8) +
+#   geom_ribbon(data = pred_df, aes(ymin = lower, ymax = upper, x = year), 
+#               alpha = 0.3, fill = "blue", inherit.aes = FALSE) +
+#   geom_line(data = pred_df, aes(y = estimate, x = year ), color = "blue", size = 1, inherit.aes = FALSE) +
+#   geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.6) +
+#   labs(x = "Year", y = "Age Difference (KP - GenPop)") +
+#   moz.utils::standard_theme()
+# 
+# 
+# 
+# #### MSM
+# 
+# test_msm <- inla_msm_age %>% filter(!is.na(survey_id)) %>% 
+#   filter(!is.na(age)) %>% 
+#   group_by(survey_id) %>% 
+#   mutate(mean_kp_age = mean(age),
+#          sd_kp_age = sd(age),
+#          denom = n()) %>% 
+#   ungroup() %>% 
+#   select(survey_id, year, genpop_mean, mean_kp_age, sd_kp_age, denom) %>% 
+#   distinct() %>% 
+#   # group_by(survey_id, age) %>% 
+#   # mutate(age_denom = n()) %>% 
+#   # ungroup() %>% 
+#   mutate(se = sd_kp_age/sqrt(denom),
+#          variance = se^2,
+#          weights = 1/variance,
+#          diff = mean_kp_age - genpop_mean,
+#          mean_year = year - 2018)  
+# 
+# 
+# weighted_model_msm <- lm(diff ~ mean_year, 
+#                      data = test_msm, 
+#                      weights = weights)
+# 
+# summary(weighted_model_msm)
+# 
+# pred_data_msm <- data.frame(year = 2010:2023) %>% 
+#   mutate(mean_year = year - 2018)
+# 
+# predictions_msm <- predict(weighted_model_msm, newdata = pred_data_msm, 
+#                        interval = "confidence", level = 0.95)
+# pred_df_msm <- data.frame(
+#   year = 2010:2023,
+#   estimate = predictions_msm[,"fit"],
+#   lower = predictions_msm[,"lwr"],
+#   upper = predictions_msm[,"upr"]
+# )
+# 
+# # Create the plot
+# test_msm %>% 
+#   ggplot() +
+#   geom_point(aes(x = year, y = diff, size = denom), alpha = 0.8) +
+#   geom_ribbon(data = pred_df_msm, aes(ymin = lower, ymax = upper, x = year), 
+#               alpha = 0.3, fill = "blue", inherit.aes = FALSE) +
+#   geom_line(data = pred_df_msm, aes(y = estimate, x = year ), color = "blue", size = 1, inherit.aes = FALSE) +
+#   geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.6) +
+#   labs(x = "Year", y = "Age Difference (KP - GenPop)") +
+#   moz.utils::standard_theme()
 
-## Generate a means 
 
-test <- inla_fsw_age %>% filter(!is.na(survey_id)) %>% 
-  filter(!is.na(age)) %>% 
-  group_by(survey_id) %>% 
-  mutate(mean_kp_age = mean(age),
-         sd_kp_age = sd(age),
-         denom = n()) %>% 
+sd_fsw <- data.frame(year = 1993:2023,
+                     denom = 1) %>% 
+  bind_rows(fsw_age_dat %>% group_by(survey_id) %>%
+  mutate(mean_kp_age2 = mean(age, na.rm = T), 
+         sd_kp_age = sd(age, na.rm = T)) %>% 
   ungroup() %>% 
-  select(survey_id, year, median_kp_age, mean_kp_age, sd_kp_age, denom, genpop_mean) %>% 
+  select(survey_id, iso3, year, mean_kp_age, sd_kp_age, genpop_mean, megamean, denom) %>% 
   distinct() %>% 
-  # group_by(survey_id, age) %>% 
-  # mutate(age_denom = n()) %>% 
-  # ungroup() %>% 
-  mutate(se = sd_kp_age/sqrt(denom),
-         variance = se^2,
-         weights = 1/variance,
-         diff = mean_kp_age - genpop_mean,
-         mean_year = year - 2017)  
+  left_join(genpop_median_ages %>% filter(sex == "female") %>% select(iso3, year, sd_age)) %>% 
+  mutate(mean_diff = mean_kp_age - genpop_mean,
+         sd_diff = sd_kp_age - sd_age)) %>% 
+  mutate(mean_year = year - 2014)
 
 
-weighted_model <- lm(diff ~ mean_year, 
-                     data = test, 
-                     weights = weights)
+sd_formula = sd_diff ~ mean_year + f(survey_id, model = "iid")
 
-summary(weighted_model)
 
-pred_data <- data.frame(year = 1993:2023) %>% 
+sd_mod <- INLA::inla(formula = sd_formula,
+                                      family = "gaussian",
+                                      control.compute=list(config = TRUE),
+                                      data = sd_fsw)
+
+summary(sd_mod)
+
+sd_samples <- moz.utils::sample_model(sd_mod, sd_fsw, col = "survey_id")
+
+sd_samples %>% 
+  ggplot() + 
+  geom_line(aes(x = year, y = mean)) + 
+  geom_ribbon(aes(x = year, ymin = lower, ymax= upper), alpha = 0.3, fill = "darkgreen") +
+  # geom_point(aes(x = year, y = sd_kp_age, size = denom)) +
+  # geom_smooth(aes(x = year, y = sd_kp_age, weight= denom), method = "lm")
+  geom_point(data = sd_fsw, aes(x = year, y = sd_diff, size = denom)) +
+  # geom_smooth(aes(x = year, y = sd_diff, weight = denom), method = "lm") +
+  labs(y = "Difference in SD") +
+  theme_minimal()
+
+
+
+sd_msm <- data.frame(year = 2010:2023,
+                     denom = 1) %>% 
+  bind_rows(msm_age_dat %>% ungroup() %>% group_by(survey_id) %>%
+              mutate(mean_kp_age2 = mean(age, na.rm = T), 
+                     sd_kp_age = sd(age, na.rm = T)) %>% 
+              ungroup() %>% 
+              select(survey_id, iso3, year, mean_kp_age, sd_kp_age, genpop_mean, megamean, denom) %>% 
+              distinct() %>% 
+              left_join(genpop_median_ages %>% filter(sex == "male") %>% select(iso3, year, sd_age)) %>% 
+              mutate(mean_diff = mean_kp_age - genpop_mean,
+                     sd_diff = sd_kp_age - sd_age)) %>% 
   mutate(mean_year = year - 2017)
-predictions <- predict(weighted_model, newdata = pred_data, 
-                       interval = "confidence", level = 0.95)
-pred_df <- data.frame(
-  year = 1993:2023,
-  estimate = predictions[,"fit"],
-  lower = predictions[,"lwr"],
-  upper = predictions[,"upr"]
-)
-
-test %>% 
-ggplot() +
-  geom_point(aes(x = year, y = diff, size = weights), alpha = 0.8) +
-  geom_ribbon(data = pred_df, aes(ymin = lower, ymax = upper, x = year), 
-              alpha = 0.3, fill = "blue", inherit.aes = FALSE) +
-  geom_line(data = pred_df, aes(y = estimate, x = year ), color = "blue", size = 1, inherit.aes = FALSE) +
-  geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.6) +
-  labs(x = "Year", y = "Age Difference (KP - GenPop)") +
-  moz.utils::standard_theme()
 
 
+sd_formula = sd_diff ~ mean_year + f(survey_id, model = "iid")
 
-#### MSM
 
-test_msm <- inla_msm_age %>% filter(!is.na(survey_id)) %>% 
-  filter(!is.na(age)) %>% 
-  group_by(survey_id) %>% 
-  mutate(mean_kp_age = mean(age),
-         sd_kp_age = sd(age),
-         denom = n()) %>% 
-  ungroup() %>% 
-  select(survey_id, year, genpop_mean, mean_kp_age, sd_kp_age, denom) %>% 
+sd_mod_msm <- INLA::inla(formula = sd_formula,
+                     family = "gau ssian",
+                     control.compute=list(config = TRUE),
+                     data = sd_msm)
+
+summary(sd_mod_msm)
+
+sd_samples_msm <- moz.utils::sample_model(sd_mod_msm, sd_msm, col = "survey_id")
+
+sd_samples_msm %>% 
+  ggplot() + 
+  geom_line(aes(x = year, y = mean)) + 
+  geom_ribbon(aes(x = year, ymin = lower, ymax= upper), alpha = 0.3, fill = "purple") +
+  # geom_point(aes(x = year, y = sd_kp_age, size = denom)) +
+  # geom_smooth(aes(x = year, y = sd_kp_age, weight= denom), method = "lm")
+  geom_point(data = sd_msm, aes(x = year, y = sd_diff, size = denom)) +
+  # geom_smooth(aes(x = year, y = sd_diff, weight = denom), method = "lm") +
+  labs(y = "Difference in SD") +
+  theme_minimal()
+
+
+
+###### PWID
+
+
+inla_fsw_age <- crossing(year = 1993:2023,
+                         iso3 = unique(fsw_age_dat$iso3),
+                         model = "full_sample") %>% 
+  bind_rows(crossing(year = 1993:2023) %>%
+              left_join(genpop_median_ages) %>% 
+              filter(sex == "female")%>%
+              select(year, sex, genpop_median = megamedian, genpop_mean = megamean) %>% 
+              distinct() %>% 
+              mutate(model = "ssa_genpop") %>% 
+              filter(sex == "female")) %>% 
+  bind_rows(crossing(year = 1993:2023) %>%
+              left_join(genpop_median_ages) %>% 
+              filter(sex == "female") %>%
+              select(year, iso3, sex, genpop_median = median_age, genpop_mean = mean_age) %>% 
+              distinct() %>% 
+              mutate(model = "country_slopes") )
+
+inla_fsw_age <- inla_fsw_age %>% 
+  select(year, model, iso3, genpop_median, genpop_mean) %>%
   distinct() %>% 
-  # group_by(survey_id, age) %>% 
-  # mutate(age_denom = n()) %>% 
-  # ungroup() %>% 
-  mutate(se = sd_kp_age/sqrt(denom),
-         variance = se^2,
-         weights = 1/variance,
-         diff = mean_kp_age - genpop_mean,
-         mean_year = year - 2018)  
+  bind_rows(fsw_age_dat) %>% 
+  filter(!age<15 | is.na(age)) %>% 
+  mutate(year_centre = year-2000) %>% 
+  mutate(year_min = year - min(year),
+         iso3_id = iso3,
+         mean_year = (year - 2017)/5)
+
+inla_fsw_age2 <- inla_fsw_age %>% 
+  filter(!year < 2005)
+
+fsw_age_formula1 <- age ~ 1 + mean_year + f(iso3, model = "iid") + f(iso3_id, mean_year, model = "iid") + offset(log(genpop_mean))
+
+fsw_agemod_gamma_offset <- INLA::inla(formula = fsw_age_formula1,
+                                      family = "gamma",
+                                      control.compute=list(config = TRUE, waic = T),
+                                      data = inla_fsw_age)
+
+summary(fsw_agemod_gamma_offset)
+
+fsw_agemod_gamma_offset_samples <- moz.utils::sample_model(fsw_agemod_gamma_offset, inla_fsw_age, col = "survey_id")
+
+(fsw_ageratio_gamma_offset <- fsw_agemod_gamma_offset_samples %>% 
+    filter(model == "ssa_genpop") %>% 
+    mutate(exp_mean = exp(mean),
+           exp_lower = exp(lower),
+           exp_upper = exp(upper)) %>% 
+    ggplot() + 
+    geom_line(aes(x = year , y = (exp(mean))-genpop_mean)) +
+    geom_ribbon(aes(x = year, ymin = (exp(lower))- genpop_mean, ymax = (exp(upper))-genpop_mean), alpha = 0.3) +
+    geom_point(data = fsw_age_dat , aes(x = year, y = mean_kp_age-genpop_mean, size = denom, color = iso3), show.legend = F) + 
+    geom_hline(yintercept = 0 , color = "darkred", linetype = "dotted", linewidth = 0.7) +
+    moz.utils::standard_theme() + 
+    labs(x = "Year", y = "Number of years older \nthan the total population") +
+    theme(aspect.ratio = 1) + 
+    annotate("segment", x = 1993, xend = 1993, y = 0.5, yend = 5.5,
+             arrow = arrow(length = unit(0.15, "inches")), color = "darkred") +
+    annotate("text", x = 1993, y = 6.7, label = "FSW older than total \npopulation women", size = 4, fontface = "bold", hjust =0 , color = "darkred") +
+    annotate("segment", x = 1993, xend = 1993, y = -0.5, yend = -5.5,
+             arrow = arrow(length = unit(0.15, "inches")), color = "darkred") +
+    annotate("text", x = 1993, y = -6.3, label = "FSW younger than total \npopulation women", size = 4, fontface = "bold", hjust =0, color = "darkred" ) + 
+    ggtitle("SSA trend")) 
 
 
-weighted_model_msm <- lm(diff ~ mean_year, 
-                     data = test_msm, 
-                     weights = weights)
 
-summary(weighted_model_msm)
 
-pred_data_msm <- data.frame(year = 2010:2023) %>% 
-  mutate(mean_year = year - 2018)
 
-predictions_msm <- predict(weighted_model_msm, newdata = pred_data_msm, 
-                       interval = "confidence", level = 0.95)
-pred_df_msm <- data.frame(
-  year = 2010:2023,
-  estimate = predictions_msm[,"fit"],
-  lower = predictions_msm[,"lwr"],
-  upper = predictions_msm[,"upr"]
-)
-
-# Create the plot
-test_msm %>% 
-  ggplot() +
-  geom_point(aes(x = year, y = diff, size = denom), alpha = 0.8) +
-  geom_ribbon(data = pred_df_msm, aes(ymin = lower, ymax = upper, x = year), 
-              alpha = 0.3, fill = "blue", inherit.aes = FALSE) +
-  geom_line(data = pred_df_msm, aes(y = estimate, x = year ), color = "blue", size = 1, inherit.aes = FALSE) +
-  geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.6) +
-  labs(x = "Year", y = "Age Difference (KP - GenPop)") +
-  moz.utils::standard_theme()
